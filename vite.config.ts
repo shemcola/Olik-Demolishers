35e762de76b6833bd5c57cef3d7bd570ebbecc85
shemcola <shem.b.cola@gmail.com>
2026-02-13 01:30:18 +0300

    Setup Vite build process to fix TSX MIME type error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
-import path from 'path';
+uimport { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
